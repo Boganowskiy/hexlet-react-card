ed2ab48b13108b5b870890991f47438fdc345f2e
Boganowskiy Andrey <boganowskiy@gmail.com>
2020-08-30 01:52:50 +0300

    Updates

---
+++ precache-manifest.5071c3d64cfe384f4d7c5318d4ed3cde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad125538f5dc997c2fd31e30726ad74c",
+    "url": "/hexlet-react-card/index.html"
+  },
+  {
+    "revision": "9340e116f447947cf612",
+    "url": "/hexlet-react-card/static/js/2.27e7a230.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hexlet-react-card/static/js/2.27e7a230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0355b5bc15e549324831",
+    "url": "/hexlet-react-card/static/js/main.900c4aea.chunk.js"
+  },
+  {
+    "revision": "981501da186b5606734c",
+    "url": "/hexlet-react-card/static/js/runtime-main.ef816f5b.js"
+  }
+]);
